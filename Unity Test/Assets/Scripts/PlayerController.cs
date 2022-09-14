ca453178be7aaa56bae047cd7e048901ba8139b0
RoySanderson <roysanderson98@gmail.com>
2022-09-14 21:10:09 +0200

    Player placeholder + third person camera

---
+++ Unity Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
